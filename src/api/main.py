4bfbed1dd39868dd9a3b5cb8ca4779f3962a19b1
Vladimir <stepanov.v@selectel.ru>
2025-04-13 18:38:38 +0300

    v1.6 fix templates

--- src/api/main.py
+++ src/api/main.py
@@ -1,4 +1,3 @@
-import uvicorn
 from fastapi import FastAPI
 from starlette.requests import Request
 from starlette.responses import HTMLResponse
@@ -10,7 +9,7 @@ from usecases import GetPlayersUseCase
 
 app = FastAPI(title="MafiaAPI")
 app.mount("/static", StaticFiles(directory="api/static"), name="static")
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="api/templates")
 
 
 @app.get("/players", description="returns players for last game in draft")
@@ -19,8 +18,3 @@ async def get_players(request: Request) -> HTMLResponse:
     players = await uc.get_players_for_stream()
     s = sorted(players, key=lambda p: p.number) if players else None
     return templates.TemplateResponse(request=request, name="main.html", context={"players": s})
-
-
-if __name__ == "__main__":
-    uvicorn.run(app="main:app", reload=True)
-
